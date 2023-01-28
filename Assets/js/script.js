5b264aabbddb172a9bf186ddd8b7472326036c74
Jesus S <jsalazar2010@gmail.com>
2023-01-27 19:08:14 -0700

    add divs, time function and event handlers

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,70 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+  var time = dayjs();
+  var headerDate = time.format("dddd, MMMM D");
+  $("#currentDay").html(headerDate);
+
+
+$(".time-block").each(function() {
+  // console.log("hello!", this);
+  var rowTime = parseInt($(this).attr("id").split("-")[1]);
+  var timeNow = parseInt(dayjs().format("H"));
+  console.log(typeof(timeNow));
+
+  if (rowTime === timeNow) {
+    $(this).addClass("present");
+  }
+  else if (rowTime > timeNow) {
+    $(this).addClass("future");
+  }
+  else {
+    $(this).addClass("past");
+  }
+});
+
+$(".saveBtn").on("click", function() {
+  console.log("this");
+  var textInput = $(this).siblings("textarea").val();
+  console.log(textInput);
+
+  var rowHour = $(this).parent().attr("id");
+  console.log(rowHour);
+
+  localStorage.setItem(rowHour, textInput);
+});
+
+
+
+for (let i = 9; i <= 17; i++) {
+  $(`#hour-${i} textarea`).val(localStorage.getItem(`hour-${i}`));
+  
+}
+
+// PSUEDO CODE HERE 
+/* 
+GIVEN I am using a daily planner to create a schedule
+
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+WHEN I click into a timeblock
+THEN I can enter an event
+
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+
+WHEN I refresh the page
+THEN the saved events persist
+ */
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
